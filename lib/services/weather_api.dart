b04bce5d2d0d210bb18984dae944f1172df7fb60
sohaibroy <157553837+sohaibroy@users.noreply.github.com>
2025-05-14 11:53:10 -0600

    sighned up for API key, did the constants, copied a return JSON from endpoint from example, created function to get weather information for the required city.

---
+++ lib/services/weather_api.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+///Constants
+const string weatherApikey = a7c59d76c25b57180487e6e05023c687
+const string currentWeatherEndpoint = "https://api.openweathermap.org/data/2.5/weather"
+
+///returning JSON from the endpoint
+Future<dynamic> getJson(String endpoint) async {
+  final res = await http.get(Uri.parse(endpoint));
+  final data = jsonDecode(res.body);
+  return data;
+}
+
+///getting current weather information from the provided city
+Future<dynamic> getWeatherForCity({required String city}) async {
+  final endpoint = "$currentWeatherEndpoint?units=metric&q=$city&appid=$weatherApiKey";
+  final data = await getJson(endpoint);
+
+  final statusCode = (data as Map<String, dynamic>)['cod'];
+  if (statusCode != 200) {
+    throw Exception("There was a problem with the request: status $statusCode received.");
+  }
+
+  return data;
+}
